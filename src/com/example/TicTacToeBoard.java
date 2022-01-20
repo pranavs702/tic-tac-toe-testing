de48bb79233344df6fb3a3a552806ee835b48f4d
pranavs702 <pranavs702@yahoo.com>
2022-01-20 15:12:12 -0800

    check board dimensions for valid board

--- src/com/example/TicTacToeBoard.java
+++ src/com/example/TicTacToeBoard.java
@@ -5,19 +5,31 @@ package com.example;
  */
 public class TicTacToeBoard {
 
+  private String board = "";
+
   /**
    * This method should load a string into your TicTacToeBoard class.
+   *
    * @param board The string representing the board
    */
   public TicTacToeBoard(String board) {
-
+    this.board = board;
   }
 
   /**
    * Checks the state of the board (unreachable, no winner, X wins, or O wins)
+   *
    * @return an enum value corresponding to the board evaluation
    */
   public Evaluation evaluate() {
-    return Evaluation.UnreachableState;
+    int x = 0;
+    int o = 0;
+    int n = (int) Math.sqrt(board.length());
+
+    if (n * n != board.length()) {
+      throw new IllegalArgumentException();
+    }
+
+    return Evaluation.NoWinner;
   }
 }
